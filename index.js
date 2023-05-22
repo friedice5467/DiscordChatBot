98b61bb31796afacfa728545f01cf34828fcfdca
Danny Nguyen <58054670+friedice5467@users.noreply.github.com>
2023-05-21 22:00:49 -0700

    made it so that you would need to ping icebot to talk to it, added ! commands

--- index.js
+++ index.js
@@ -1,66 +1,84 @@
-const keepAlive = require("./server")
+const keepAlive = require("./server");
 const fetch = require('node-fetch');
-const { Client, GatewayIntentBits } = require('discord.js')
+const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.DirectMessages,
-      GatewayIntentBits.DirectMessageTyping,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.GuildMessageTyping,
-      GatewayIntentBits.DirectMessageReactions,
-      GatewayIntentBits.MessageContent
-    ]
-})
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.DirectMessageReactions,
+    GatewayIntentBits.MessageContent
+  ]
+});
 
 API_URL = 'https://api-inference.huggingface.co/models/microsoft/DialoGPT-large';
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+  console.log(` client user id is ${client.user.id}`);
+  console.log(`Logged in as ${client.user.tag}!`);
 
-client.on("messageCreate", async (message) => {
-
+  client.on("messageCreate", async (message) => {
     if (message.author.bot) {
-        return;
-    }
-      
-    if(message.channel.name != 'bot-channel')
-    {
       return;
     }
-  console.log(message.content);
-  
-    const payload = {
-        inputs: {
-            text: message.content
-        }
-    };
 
-  const headers = {
-       "Authorization": process.env['aiToken']
-    };
+    console.log(message.content);
 
-  message.channel.sendTyping();
-
-  const response = await fetch(API_URL, {
-        method: 'post',
-        body: JSON.stringify(payload),
-        headers: headers
-    });
-
-  const data = await response.json();
     let botResponse = '';
-    if (data.hasOwnProperty('generated_text')) {
-        botResponse = data.generated_text;
-    } else if (data.hasOwnProperty('error')) { // error condition
-        botResponse = data.error;
+
+    const words = message.content.split(/\s+/);
+    const commandWord = words.find(word => word.startsWith('!'));
+
+    if (commandWord) {
+      // the message contains a command, process it
+      let command = commandWord.trim().substring(1); // Remove "!" prefix
+      if (command === "summon") {
+        botResponse = "<:hmmcouncil1:1108159415632281602><:hmmcouncil2:1108159413598044190><:hmmcouncil3:1108159412285231155><:hmmcouncil4:1108159410343264257><:hmmcouncil5:1108159408694894653>";
+      }
+      else {
+        botResponse = "Command not found";
+      }
+    } else if (message.mentions.users.has(client.user.id)) {
+      // The bot was mentioned, but the message is not a command
+      let messageContent = message.content.replace(`<@${client.user.id}>`, "").trim();
+      if (messageContent !== '') {
+        const payload = {
+          inputs: {
+            text: messageContent
+          }
+        };
+
+        const headers = {
+          "Authorization": process.env['aiToken']
+        };
+
+        message.channel.sendTyping();
+
+        const response = await fetch(API_URL, {
+          method: 'post',
+          body: JSON.stringify(payload),
+          headers: headers
+        });
+
+        const data = await response.json();
+
+        if (data.hasOwnProperty('generated_text')) {
+          botResponse = data.generated_text;
+        } else if (data.hasOwnProperty('error')) { // error condition
+          botResponse = data.error;
+        }
+      }
+    } else {
+      // The bot was not mentioned, and the message is not a command
+      return;
     }
-    // stop typing
+
     message.channel.sendTyping();
-    // send message to channel as a reply
     message.reply(botResponse);
+  });
 });
 
-keepAlive()
-client.login(process.env['discToken'])
+keepAlive();
+client.login(process.env['discToken']);
